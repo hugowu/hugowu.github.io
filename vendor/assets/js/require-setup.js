33928befd58360ebaabe23c21f35ff0d7711e7c6
hugowu <wuwenqing@aliyun.com>
2016-12-24 20:03:52 +0800

    test

--- vendor/assets/js/require-setup.js
+++ vendor/assets/js/require-setup.js
@@ -4,12 +4,12 @@
 // To config RequireJS after it’s loaded, pass the below object into require.config();
 
 var require = {
-     //baseUrl:'.',
+    baseUrl:'../../',
     shim : {
         "bootstrap" : { "deps" :['jquery'] }
     },
     paths: {
-        "jquery" : ["//apps.bdimg.com/libs/jquery/2.1.4/jquery.min","//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.min","vendor/assets/js/jquery.min"],
+        "jquery" : ["//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.min","//apps.bdimg.com/libs/jquery/2.1.4/jquery.min","vendor/assets/js/jquery.min"],
         "bootstrap" :  ["//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.min","//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min","vendor/bootstrap-3.3.7/dist/js/bootstrap.min"],
         //"workaround" : "vendor/assets/js/ie10-viewport-bug-workaround"
     }
