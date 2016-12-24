2f27b1953abbfd03e9d40150d58690bf3cc47819
hugowu <wuwenqing@aliyun.com>
2016-12-24 15:12:13 +0800

    test

--- vendor/assets/js/require-setup.js
+++ vendor/assets/js/require-setup.js
@@ -9,8 +9,8 @@ var require = {
         "bootstrap" : { "deps" :['jquery'] }
     },
     paths: {
-        "jquery" : "//cdn.bootcss.com/jquery/2.1.4/jquery.min1",
-        "bootstrap" :  "//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min1",
+        "jquery" : "//cdn.bootcss.com/jquery/2.1.4/jquery.min",
+        "bootstrap" :  "//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min",
         "workaround" : "vendor/assets/js/ie10-viewport-bug-workaround"
     }
 };
